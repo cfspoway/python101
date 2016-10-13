db2c9cf48b2da2223a8b8d9dcc151fca8f71b50d
gangding <gangding@gmail.com>
2016-10-13 11:36:39 -0700

    Add files via upload

---
+++ Homework03/Class_Notes_2016-10-13.py
@@ -0,0 +1,15 @@
+# Ask for input of a number
+# Give an output of number * 999
+
+# step 1: Ask user to provide a number
+inputString = input('Please give a number: ')
+
+# step 2: Computer process the input data
+# Convert input string into an integer number:
+number = float(inputString)
+# Computer does the math:
+result = number * 999
+
+# step 3: Provide the result to the user
+print(str(number) + ' * 999 = ' + str(result))
+
