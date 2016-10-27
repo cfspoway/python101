867f90d2049006e18dcea1d8463c45a080a41ced
gangding <gangding@gmail.com>
2016-10-27 12:33:18 -0700

    Add files via upload

---
+++ class2016-10-27/ClassNote_20161027.py
@@ -0,0 +1,81 @@
+>>> x = 10
+>>> x
+10
+>>> x+5
+15
+>>> y=x+5
+>>> y
+15
+>>> y = x + 10
+>>> y
+20
+>>> x = x + 10
+>>> x
+20
+>>> x = x * 10
+>>> x
+200
+>>> x = x * 10
+>>> x
+2000
+>>> x = 99
+>>> x = x * 10
+>>> x
+990
+>>> x = (x - 9) * 99 - 3 ** 2
+>>> x
+97110
+>>> x = x + 1
+>>> x
+97111
+>>> 5 > 3
+True
+>>> 100 > 101
+False
+>>> Compare5and3 = 5 > 3
+>>> Compare5and3
+True
+>>> CompareAgain = 100 > 101
+>>> CompareAgain
+False
+>>> x=0
+>>> x
+0
+>>> x = 0
+>>> x
+0
+>>> x == 0
+True
+>>> 3**3
+27
+>>> x == 0
+True
+>>> (3**3) == 21
+False
+>>> (3**3) != 21
+True
+>>> UserName = "Alice"
+>>> UserName
+'Alice'
+>>> UserName == "Alice"
+True
+>>> UserName != "Alice"
+False
+>>> UserName = "Bob"
+>>> UserName
+'Bob'
+>>> UserName == "Alice"
+False
+>>> UserName != "Alice"
+True
+>>> x = 10
+>>> x >= 10
+True
+>>> x <= 10
+True
+>>> BooleanResult = 100 > 101
+>>> BooleanResult
+False
+>>> print('100 > 101 is', BooleanResult)
+100 > 101 is False
+
