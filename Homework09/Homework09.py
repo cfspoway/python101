dc6d97ab45ee118d18ad7b47d7a23ea09779ce66
gangding <gangding@gmail.com>
2017-01-19 21:52:33 -0800

    Update Homework09.py

--- Homework09/Homework09.py
+++ Homework09/Homework09.py
@@ -4,11 +4,11 @@ num1 = 0
 num2 = 1
 print(str(num1) + '\n' + str(num2))
 maxnum = 1000
-count = 2
+count = 1
 while num2 <= maxnum:
-   num3 = num1 + num2
-   print(str(num3))
+   print(str(num2))
    count = count + 1
+   num3 = num1 + num2
    num1 = num2
    num2 = num3
     
